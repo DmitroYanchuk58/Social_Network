02f4a4d26b7b327885b04ba951bd7da4fb17ba40
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-01-07 17:55:50 +0200

    Make database context

---
+++ DAL/DatabaseContext/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.DatabaseContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext() : base()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-CPI51I3\MSSQLSERVER01;Initial Catalog=SocialNetworkDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+        }
+    }
+}
