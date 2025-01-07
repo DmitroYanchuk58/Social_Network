9a27363110b4d0fa052d596aba951b6d24f28707
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-01-07 17:29:46 +0200

    Make User entity in DAL

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DAL.Entities
+{
+    public class User : Entity
+    {
+        [Required]
+        public string Nickname {  get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
