e4fd9e4848d6a93f2c08ce08ddc8dc3ae953b7a6
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-01-20 18:00:05 +0200

    Add PL auth part

---
+++ WebApp_PL/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using BAL.Services;
+using BAL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_PL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        IUserService _service;
+        public AuthController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string email, string password)
+        {
+            var isAuthenticated = _service.Authentication(email, password);
+            if (isAuthenticated)
+            {
+                return Ok(new { Message = "Login successful" });
+            }
+            return Unauthorized(new { Message = "Invalid credentials" });
+        }
+
+        [HttpPost]
+        public IActionResult Registration(string email, string password, string nickname)
+        {
+            try
+            {
+                _service.Registration(email, password, nickname);
+                return Ok(new { Message = "Registration successful" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = "An error occurred during registration", Details = ex.Message });
+            }
+        }
+    }
+}
--- WebApp_PL/Program.cs
+++ WebApp_PL/Program.cs
@@ -1,3 +1,9 @@
+using BAL.Services;
+using BAL.Services.Interfaces;
+using DAL.DatabaseContextNamespace;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Database
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(@"Data Source=desktop-cpi51i3;Initial Catalog=SocialNetworkDatabase;Integrated Security=True;Encrypt=True;Trust Server Certificate=True")));
+//Custom services
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
