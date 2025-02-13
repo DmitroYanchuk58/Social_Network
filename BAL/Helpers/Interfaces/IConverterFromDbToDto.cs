d5afe4b69ed33415eff7e39c11aa1f735bfae334
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-02-13 19:35:19 +0200

    Add in and out

--- BAL/Helpers/Interfaces/IConverterFromDbToDto.cs
+++ BAL/Helpers/Interfaces/IConverterFromDbToDto.cs
@@ -3,7 +3,7 @@ using EntityDTO = BAL.DTOs.IEntity;
 
 namespace BAL.Helpers.Interfaces
 {
-    public interface IConverterFromDbToDto<T, S>
+    public interface IConverterFromDbToDto<in T, out S>
         where T : EntityDB
         where S : EntityDTO
     {
