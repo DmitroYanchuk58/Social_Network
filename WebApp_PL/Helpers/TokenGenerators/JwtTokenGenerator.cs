5332e23dd7452dc5ac99e893d8666a98059a997c
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-02-13 17:05:53 +0200

    Change jwt token logic 3

--- WebApp_PL/Helpers/TokenGenerators/JwtTokenGenerator.cs
+++ WebApp_PL/Helpers/TokenGenerators/JwtTokenGenerator.cs
@@ -9,28 +9,30 @@ namespace WebApp_PL.Helpers
     {
         public static string GenerateToken(string username, IConfiguration configuration)
         {
-            var claims = new[]
-            {
-            new Claim(JwtRegisteredClaimNames.Sub, username),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
-
-            var secretKey = configuration["JwtSettings:SecretKey"]; 
+            var secretKey = configuration["JwtSettings:SecretKey"];
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
 
             var token = new JwtSecurityToken(
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(30),
-                signingCredentials: creds);
+                signingCredentials: creds
+            );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
+
         public static SymmetricSecurityKey GetKey(IConfiguration configuration)
         {
             var secretKey = configuration["JwtSettings:SecretKey"];
-            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]));
         }
     }
 }
