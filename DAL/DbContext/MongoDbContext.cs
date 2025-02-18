a5a86fdfe30b3003139f6ccb040e9fd6e217e84c
Dmitro Yanchuk <129006113+Dimochka5@users.noreply.github.com>
2025-02-18 16:40:00 +0200

    Changed Jwt token logic

---
+++ DAL/DbContext/MongoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Entities.MongoDbEntities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DbContext
+{
+    public class MongoDbContext
+    {
+        private IMongoDatabase database;
+        public MongoDbContext() 
+        {
+            var connectionString = "mongodb+srv://Dima:55555@cluster0.7hu6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+            var mongoClient = new MongoClient(connectionString);
+            database = mongoClient.GetDatabase("SocialNetwork");
+        }
+
+        public string GetJwtSecretKey()
+        {
+            var keys = database.GetCollection<JwtSecretKey>("JwtSecretKey");
+            var firstKey = keys.Find(_ => true).First();
+            var secretKey = firstKey.SecretKey;
+            return secretKey;
+        }
+    }
+}
---
+++ DAL/Entities/MongoDbEntities/JwtSecretKey.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.MongoDbEntities
+{
+    [Serializable]
+    public class JwtSecretKey
+    {
+        [BsonId, BsonElement("id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("secret_key"), BsonRepresentation(BsonType.String)]
+        public string SecretKey { get; set; }
+    }
+}
